9a12a75359fcd8b5e01103ff27b14fe2b11e822c
Avnoor Kaur <Avnoork2004@gmail.com>
2024-11-19 22:08:48 -0500

    modified the signup page look wise (just added text and textfields)

--- src/main/java/viewmodel/SignUpController.java
+++ src/main/java/viewmodel/SignUpController.java
@@ -1,14 +1,37 @@
 package viewmodel;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+
 public class SignUpController {
+
+    //added methods from the modified fxml file
+    @FXML
+    private Button goBackBtn;
+
+    @FXML
+    private Button newAccountBtn;
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private PasswordField confirmPasswordField;
+    @FXML
+    private TextField emailField;
+
+
     public void createNewAccount(ActionEvent actionEvent) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setContentText("Info for the user. Message goes here");
