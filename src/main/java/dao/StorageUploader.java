cdd1017c09b9b7124961d555576881fb5af706e2
Avnoor Kaur <Avnoork2004@gmail.com>
2024-11-21 17:21:01 -0500

    added a comment in the stoargeuploader so if u replace the placeholder with the (cString), the load bar fully loads now when you upload the profile pic. (gets stored in the media-files container

--- src/main/java/dao/StorageUploader.java
+++ src/main/java/dao/StorageUploader.java
@@ -26,6 +26,7 @@ public class StorageUploader {
                 .buildClient();
     }
 
+
     public void uploadFile(String filePath, String blobName) {
         BlobClient blobClient = containerClient.getBlobClient(blobName);
         blobClient.uploadFromFile(filePath);
